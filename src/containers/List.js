f0f112366306f449841f434fca50d4903f884499
Akhilesh <akhileshshahare@gmail.com>
2019-05-04 18:16:15 +0530

    Added infinte scrolling

--- src/containers/List.js
+++ src/containers/List.js
@@ -1,35 +1,49 @@
 import React, { Component } from "react";
 const API_KEY = "AIzaSyAlPlr6ZuMz9LdvfhrHoqI2dB6IbDNBJjA";
 
-
 const debounce = (fn, time) => {
   let timeout;
 
   return function() {
     const functionCall = () => fn.apply(this, arguments);
-    
+
     clearTimeout(timeout);
     timeout = setTimeout(functionCall, time);
-  }
-}
+  };
+};
 export class List extends Component {
   state = {
-    filtered: []
+    filtered: [],
+    searchText: "",
+    isLoading: false
   };
 
-  fetchVideos = debounce(async query => {
-    await fetch(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&type=video&key=${API_KEY}`
-    )
+  fetchVideosApi = async (query, loadNext) => {
+    let url = "";
+    this.setState({ isLoading: true });
+
+    if (loadNext)
+      url = `https://www.googleapis.com/youtube/v3/search?pageToken=CBkQAA&part=snippet&maxResults=35&q=${query}&type=video&key=${API_KEY}`;
+    url = `https://www.googleapis.com/youtube/v3/search?&part=snippet&maxResults=35&q=${query}&type=video&key=${API_KEY}`;
+    await fetch(url)
       .then(results => results.json())
       .then(response => {
-        this.setState({ filtered: response.items });
+        if (!this.state.filtered.length)
+          this.setState({ filtered: response.items, isLoading: false });
+        this.setState({
+          filtered: [...this.state.filtered, ...response.items],
+          isLoading: false
+        });
       });
+  };
+
+  fetchVideos = debounce(query => {
+    this.fetchVideosApi(query);
   }, 2000);
 
   handleChange = e => {
     let newList = [];
-
+    this.setState({ searchText: e.target.value });
     // If the search bar isn't empty
     if (e.target.value !== "") {
       this.fetchVideos(e.target.value);
@@ -39,6 +53,23 @@ export class List extends Component {
       this.setState({
         filtered: newList
       });
+    }
+  };
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.onScroll, false);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.onScroll, false);
+  }
+
+  onScroll = () => {
+    if (
+      window.innerHeight + window.scrollY >= document.body.offsetHeight - 500 &&
+      this.state.filtered.length
+    ) {
+      this.fetchVideosApi(this.state.searchText, true);
     }
   };
 
@@ -52,9 +83,13 @@ export class List extends Component {
           placeholder="Search..."
         />
         <ul>
-          {this.state.filtered && this.state.filtered.map((item, index) => (
-            <li key={index}>{item.snippet.title} &nbsp;</li>
-          ))}
+          {this.state.filtered && !this.state.isLoading ? (
+            this.state.filtered.map((item, index) => (
+              <li key={index}>{item.snippet.title} &nbsp;</li>
+            ))
+          ) : (
+            <p>Loading ...</p>
+          )}
         </ul>
       </div>
     );
